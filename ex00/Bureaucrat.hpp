501c59c2f2eca2b61b7a72c2bc3a6f25c92df427
Gita Shekari <gshekari@f0r3s26.codam.nl>
2026-07-01 21:54:26 +0200

    update all

--- ex00/Bureaucrat.hpp
+++ ex00/Bureaucrat.hpp
@@ -5,15 +5,31 @@
 
 class Bureaucrat
 {
-    private:
-       const std::string name;
-       int grade;
-    public:
-        Bureaucrat(const std::string name, int grade);
-        ~Bureaucrat();
-        std::string const getName() const;
-        int getGrade();
-        
-        
+	private:
+	   const std::string name;
+	   int grade;
+	public:
+		Bureaucrat(const std::string name, int grade);
+		Bureaucrat(const Bureaucrat &other);
+		Bureaucrat &operator=(const Bureaucrat &other);
+		~Bureaucrat();
+		const std::string getName() const;
+		int getGrade() const;
+		void setGrade(int g);
+		void incrementGrade();
+		void decrementGrade();
+
+		class GradeTooHighException : public std::exception
+		{
+			const char* what() const throw();
+		};
+
+		class GradeTooLowException : public std::exception
+		{
+			const char* what() const throw();
+		};
+
 };
+std::ostream &operator<<(std::ostream &out, const Bureaucrat &bureaucrat);
+
 #endif
